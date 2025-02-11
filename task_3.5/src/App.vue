9123a598f8c725a659d353dada8f2c09ddec6bc9
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-02-11 11:13:19 +0200

    Add stylization

--- task_3.5/src/App.vue
+++ task_3.5/src/App.vue
@@ -1,12 +1,129 @@
 <template>
-    <header class="header" :style="{backgroundColor: theme.headerColor}">
-        <div class="wrapper block">
-            <button @click="theme.toggleTheme">{{ theme.currentTheme }}</button>
+    <header
+        class="header"
+        :style="{ backgroundColor: theme.headerColor }"
+    >
+        <div class="wrapper block header-content">
+            <div class="header-sub_content">
+                <img
+                    :src="theme.logoUrl"
+                    alt="Logo not found"
+                />
+                <input type="text" class="search-box" placeholder="Search by name" :style="{backgroundImage: `url(${theme.searchIconUrl})`, borderColor: theme.borderColor, backgroundColor: theme.elementColor}">
+                <button class="settings-box" :style="{borderColor: theme.borderColor, backgroundImage: `url(${theme.settingsIconUrl})`, backgroundColor: theme.elementColor}"></button>
+            </div>
+            <button
+                @click="theme.toggleTheme"
+                class="theme-picker"
+                :style="{ backgroundImage: `url(${theme.themeIconUrl})` }"
+            ></button>
         </div>
     </header>
-    <main class="main cards" :style="{backgroundColor: theme.bodyColor}">
-        <div class="wrapper block">
-            <div class="card"></div>
+    <main
+        class="main"
+        :style="{ backgroundColor: theme.bodyColor }"
+    >
+        <div class="wrapper block cards">
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
+            <div
+                class="card"
+                :style="{ borderColor: theme.borderColor }"
+            ></div>
         </div>
     </main>
 </template>
@@ -18,12 +135,10 @@ import { useThemeStore } from '@/stores/theme';
 export default defineComponent({
     data() {
         return {
-            theme: useThemeStore()
-        }
+            theme: useThemeStore(),
+        };
     },
-    methods: {
-
-    },
+    methods: {},
 });
 </script>
 
--- task_3.5/src/stores/theme.ts
+++ task_3.5/src/stores/theme.ts
@@ -2,8 +2,10 @@ import { defineStore } from 'pinia';
 
 const Theme = {
     light: 'light',
-    dark: 'dark'
+    dark: 'dark',
 } as const;
+
+/* TODO: MAKE CONSTANT OBJECTS OF THEMES */
 
 export type Theme = (typeof Theme)[keyof typeof Theme];
 
@@ -11,28 +13,41 @@ export const useThemeStore = defineStore('theme', {
     state: () => {
         return {
             currentTheme: Theme.light as Theme,
-            logoUrl: '',
+            logoUrl: '/src/assets/icons/logo-light_theme.svg',
+            settingsIconUrl: '/src/assets/icons/settings-light_theme.svg',
+            searchIconUrl: '/src/assets/icons/search-light_theme.svg',
+            themeIconUrl: '/src/assets/icons/theme_picker-light_theme.svg',
             headerColor: 'white',
             bodyColor: '#F2F5F7',
+            elementColor: '#FFFFFF',
             borderColor: '#DEDFE5',
-        }
+        };
     },
     actions: {
         toggleTheme() {
             if (this.currentTheme === Theme.light) {
-                 this.currentTheme = Theme.dark;
-                 
-                 this.headerColor = '#313131';
-                 this.bodyColor = '#222222';
-                 this.borderColor = '#646464';
-            }
-            else if (this.currentTheme === Theme.dark) {
+                this.currentTheme = Theme.dark;
+
+                this.logoUrl = '/src/assets/icons/logo-dark_theme.svg';
+                this.settingsIconUrl = '/src/assets/icons/settings-dark_theme.svg',
+                this.themeIconUrl = '/src/assets/icons/theme_picker-dark_theme.svg';
+                this.searchIconUrl = '/src/assets/icons/search-dark_theme.svg',
+                this.headerColor = '#313131';
+                this.bodyColor = '#222222';
+                this.elementColor = '#454545';
+                this.borderColor = '#646464';
+            } else if (this.currentTheme === Theme.dark) {
                 this.currentTheme = Theme.light;
 
+                this.logoUrl = '/src/assets/icons/logo-light_theme.svg';
+                this.settingsIconUrl = '/src/assets/icons/settings-light_theme.svg',
+                this.themeIconUrl = '/src/assets/icons/theme_picker-light_theme.svg';
+                this.searchIconUrl = '/src/assets/icons/search-light_theme.svg',
                 this.headerColor = 'white';
                 this.bodyColor = '#F2F5F7';
+                this.elementColor = '#FFFFFF';
                 this.borderColor = '#DEDFE5';
             }
-        }
-    }
+        },
+    },
 });
